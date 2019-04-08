f6c6cb4d3a9199ee37a4461b9a3aaf0c0c966803
Apolline Weirich <weirich_apolline@msn.com>
2019-04-08 15:23:48 +0200

    define picture contraints part 1

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PictureRepository")
@@ -21,6 +22,8 @@ class Picture
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(min="3", max="32")
      */
     private $title;
 
